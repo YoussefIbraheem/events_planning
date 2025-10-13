e35d3bd47a476538de4f5470a925eba5fc3dfc46
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-10-13 21:52:42 +0300

    remove unwanted code

--- events_planning_django/app/factories.py
+++ events_planning_django/app/factories.py
@@ -80,7 +80,7 @@ class TicketFactory(AbstractFactory):
             "attendee": kwargs.get(
                 "attendee", None
             ),  # Should be set to a valid attendee user instance
-            "ticket_price": kwargs.get("ticket_price", 0),
+            # "ticket_price": kwargs.get("ticket_price", 0),
         }
 
 
