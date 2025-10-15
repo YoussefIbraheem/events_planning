ae4138c2ae2a5452518d2aa35ccec3a3a8f21cd4
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-10-16 02:22:18 +0300

    ..continue caching logic implementation

--- events_planning_django/events_planning_django/settings.py
+++ events_planning_django/events_planning_django/settings.py
@@ -87,6 +87,17 @@ REST_FRAMEWORK = {
 }
 
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
+
+
 WSGI_APPLICATION = "events_planning_django.wsgi.application"
 
 SPECTACULAR_SETTINGS = {
