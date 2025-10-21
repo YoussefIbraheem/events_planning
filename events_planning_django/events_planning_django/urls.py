81da450536ccc9edb776ee1807d4b6c1c2ca813e
YoussefIbraheem <youssefm.muhammed@gmail.com>
2025-10-21 21:38:12 +0300

    Refactor URL patterns for improved readability and organization

--- events_planning_django/events_planning_django/urls.py
+++ events_planning_django/events_planning_django/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
+from drf_spectacular.views import SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/clearcache/', include('clearcache.urls')),
-    path('admin/', admin.site.urls),
-    path('api/', include('app.apis.urls'))
+    path("admin/clearcache/", include("clearcache.urls")),
+    path("admin/", admin.site.urls),
+    path(
+        "",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("api/", include("app.apis.urls")),
 ]
